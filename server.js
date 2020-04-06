5b012acd7a3029e0a5840bc232f4ee81229a82fb
Jaxon Potter <jaxonpotter@gmail.com>
2020-04-06 10:02:22 -0600

    add server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const { NODE_ENV } = require('./config')
+
+const app = express()
+
+const morganOption = (NODE_ENV === 'production')
+
